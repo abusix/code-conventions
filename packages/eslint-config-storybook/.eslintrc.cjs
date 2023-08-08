0272e7a8d9934abae093500f2c1cb423044ff8fe
Joseph Ribbe <joseph.ribbe@abusix.com>
2023-08-08 17:55:13 +0200

    added eslint-config-storybook package

---
+++ packages/eslint-config-storybook/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+    },
+    extends: [
+        'plugin:storybook/recommended'
+    ],
+    overrides: [
+        {
+            files: ['*.stories.tsx'],
+            rules: {
+                'no-restricted-exports': 0,
+            },
+        },
+    ],
+};
